14ddafbe5cda86b19335e11cd1a091ea3373a658
nigeld3v <32252985+nigeld3v@users.noreply.github.com>
2017-10-23 19:56:32 -0400

    basicmath.py
    This file contains a basic introduction to math functions in python.

---
+++ basicmath.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# basicmath.py
+# This document is to show how we write basic calculator functions
+# in Python.
+
+#Add 3 and 3
+print(3 + 3)
+
+# Subtract 6 from 7
+print(7 - 6)
+
+# Multiply 5 times 3
+print(5 * 3)
+
+# Divide 15 by 5
+print(15 / 5)
+
+# Find the integer remainder after dividing 16 by 3
+print(16 % 3)
+
+# What is 8 squared?
+print(8 ** 2)
+
+# What is 4 cubed?
+print(4 ** 3)
+
+# Functions follow PEM/D A/S (order of operations)
+print((((5 + 4) ** 4) - 33 / 11))
