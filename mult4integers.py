f3225ab79da59b1fa45d6c06e3d2122e5a5cc5f8
nigeld3v <32252985+nigeld3v@users.noreply.github.com>
2017-10-23 21:17:35 -0400

    mult4integers.py

---
+++ mult4integers.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Multiply 4 Integers
+# This is a beginner Python script that allows the user to multiply 4 integers
+# while launching the script in terminal. Follow the instructions in the comments below!
+
+from sys import argv
+
+# The module "argv" collects 4 arguments entered with the launch of 
+# this Python script
+
+# NOTE In terminal:
+#     Launch this file with Python3.6 mult4integers.py num1 num2 num3 num4
+#     with integers in place of the "num#" arguments.
+
+script, num1, num2, num3, num4 = argv
+
+print(int(num1) * int(num2) * int(num3) * int(num4))
